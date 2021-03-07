21fc07d5a9425a963453f4d6a863a020ca4af7cf
WillTechX20 <75710436+WillTechX20@users.noreply.github.com>
2021-03-07 15:23:16 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log('Console Web App');
+console.log('Will '.concat('Okoye'));
+
+var firstNum=1;
+var secondNum=2;
+
+console.log(firstNum+secondNum);
+console.log(secondNum+firstNum);
+console.log(firstNum-secondNum);
+console.log(secondNum-firstNum);
+console.log(firstNum*secondNum);
+console.log(secondNum*firstNum);
+console.log(firstNum/secondNum);
+console.log(secondNum/firstNum);
+console.log(Math.pow(firstNum, secondNum));
+console.log(Math.pow(secondNum, firstNum));
